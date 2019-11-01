9c586ab7a4f428429cded15df2afb66f99aa6fc8
smetanova-ys <smetanova.ys@gmail.com>
2019-11-01 15:49:14 +0100

    first task

---
+++ variables/main.js
@@ -0,0 +1,33 @@
+console.log('Задача № 1');
+
+let algebra;
+let geography;
+let physics;
+
+algebra = 5;
+geography = 4;
+physics = 4;
+
+let mean = (algebra + geography + physics) / 3;
+console.log(mean);
+console.log();
+
+console.log('Задача № 2');
+
+let myName = 'Юлия';
+let message = `Привет, мир! Меня зовут ${myName}.`;
+console.log(message);
+console.log();
+
+console.log('Задача № 3');
+
+let x;
+let y;
+let z;
+
+x = 2;
+y = 22;
+z = 0;
+
+result = x * y + 5 * z + x - 1;
+console.log(result);
